e90bbe28889d0a7541be55bd710e488b19e523b5
AnarAskar <anaraskar356@gmail.com>
2025-03-19 15:03:58 +0300

    Added new component Sidebar and used it in BrowseBlogs

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Sidebar() {
+
+    return (
+        <h1>Peshang is a sidebar</h1>
+    )
+}
+
+export default Sidebar
--- frontend/src/pages/BrowseBlogs.jsx
+++ frontend/src/pages/BrowseBlogs.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 function BrowseBlogs() {
   return (
     <div>
       <Navbar />
+
+      <Sidebar />
     </div>
   );
 }
